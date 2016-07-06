6649b656685da70c60eb8da5ae63390afec3ca72
htan712 <hock.tan712@gmail.com>
2016-07-06 12:22:00 -0700

    assignment 7 complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -31,14 +31,25 @@
             currentSong = song;
         };
         
+        /**
+        * @function playSong
+        * @desc Plays audio file currentBuzzObject and set playing == true
+        * @param {Object} song
+        */
+        
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+        
+        
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
+                    playSong(song);
                 }
             }     
         };
