6f08b96de88cc939cfac98e3863deaf06aaae741
htan712 <hock.tan712@gmail.com>
2016-07-06 11:57:13 -0700

    checkpoint 7 complete

--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -1,7 +1,7 @@
 (function(){
     function AlbumCtrl(Fixtures, SongPlayer){
         this.albumData = Fixtures.getAlbum();
-        this.songplayer = SongPlayer;
+        this.songPlayer = SongPlayer;
     }
     
     angular
